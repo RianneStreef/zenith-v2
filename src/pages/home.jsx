31e0075948e11e8df0d8e976cb2cdfc77f6e0f18
Rianne Streef <56265374+RianneStreef@users.noreply.github.com>
2023-02-14 11:51:23 +0100

    languageToUse on page home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
 import { Helmet } from "react-helmet";
+import { content } from "../content/languages";
 
-const home = () => {
+const home = (props) => {
+  let { language, languageToUse } = props;
+
+  language === "english" ? (languageToUse = content.english) : null;
+  language === "french" ? (languageToUse = content.french) : null;
+  language === "dutch" ? (languageToUse = content.dutch) : null;
   return (
     <div className="home">
       <Helmet
